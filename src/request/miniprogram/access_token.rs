0247b23caa0f06e9787a847b5e20a8c518aaac23
yansongda <me@yansongda.cn>
2025-01-10 22:08:26 +0800

    update

--- src/request/miniprogram/access_token.rs
+++ src/request/miniprogram/access_token.rs
@@ -1,10 +1,80 @@
+use std::fmt::{Display, Formatter};
 use crate::model::miniprogram::wechat_access_token::AccessToken;
 use crate::model::result::Error;
 use crate::request::Validator;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde::de::Visitor;
+
+#[derive(Debug, Clone)]
+pub enum Platform {
+    Wechat,
+    Huawei,
+    Unsupported,
+}
+
+impl Display for Platform {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Platform::Wechat => String::from("wechat"),
+            Platform::Huawei => String::from("huawei"),
+            _ => String::from("unsupported"),
+        };
+        write!(f, "{}", str)
+    }
+}
+
+impl From<String> for Platform {
+    fn from(v: String) -> Self {
+        match v.to_lowercase().as_str() {
+            "wechat" => Platform::Wechat,
+            "huawei" => Platform::Huawei,
+            _s => Platform::Unsupported,
+        }
+    }
+}
+
+impl From<Platform> for String {
+    fn from(v: Platform) -> Self {
+        v.to_string()
+    }
+}
+
+struct PlatformVisitor;
+
+impl<'de> Visitor<'de> for PlatformVisitor {
+    type Value = Platform;
+
+    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_str("反序列化失败，值应该为 string/str.")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(v.to_owned().into())
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(v.into())
+    }
+}
+
+impl<'de> Deserialize<'de> for Platform {
+    fn deserialize<D>(deserializer: D) -> Result<Platform, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_any(PlatformVisitor)
+    }
+}
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct LoginRequest {
+    pub platform: Platform,
     pub code: Option<String>,
 }
 
@@ -12,6 +82,7 @@ impl Validator for LoginRequest {
     type Data = String;
 
     fn validate(&self) -> crate::model::result::Result<Self::Data> {
+
         if self.code.is_none() {
             return Err(Error::Params(Some("小程序错误：登录秘钥不能为空")));
         }
