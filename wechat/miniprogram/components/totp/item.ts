f86f3206dc54820265952970ad68da1af40e0c53
yansongda <me@yansongda.cn>
2025-06-20 23:52:14 +0800

    optimize: 优化代码,抽离 totp 组件

---
+++ wechat/miniprogram/components/totp/item.ts
@@ -0,0 +1,67 @@
+import api from "@api/totp";
+import type { HttpError } from "@models/error";
+
+Component({
+  properties: {
+    item: {
+      type: Object,
+    },
+  },
+
+  data: {
+    remainSeconds: 0,
+    refreshCodeTimeoutIdentity: -1,
+    countdownIntervalIdentity: -1,
+  },
+
+  lifetimes: {
+    attached() {
+      console.log("attached: ", this.data.item);
+      this.countdownRefresh();
+    },
+    detached() {
+      console.log("detached: ", this.data.item);
+      this.clear();
+    },
+  },
+
+  methods: {
+    countdownRefresh() {
+      this.clear();
+
+      const period = this.data.item.config.period ?? 30;
+      const now = new Date();
+      const remainSeconds = period - (now.getSeconds() % period);
+      const msUntilNextPeriod = remainSeconds * 1000 - now.getMilliseconds();
+
+      this.data.refreshCodeTimeoutIdentity = setTimeout(() => {
+        this.refreshCode(this.data.item.id);
+        this.countdownRefresh();
+      }, msUntilNextPeriod);
+
+      let countdown = remainSeconds;
+      this.setData({ remainSeconds: countdown });
+      this.data.countdownIntervalIdentity = setInterval(() => {
+        countdown--;
+        if (countdown <= 0) {
+          clearInterval(this.data.countdownIntervalIdentity);
+        }
+        this.setData({ remainSeconds: countdown });
+      }, 1000);
+    },
+    refreshCode(id: number) {
+      api
+        .detail(id)
+        .then((response) => this.setData({ code: response.code }))
+        .catch((e: HttpError) => this.triggerEvent("refreshCodeFailed", e));
+    },
+    clear() {
+      if (this.data.refreshCodeTimeoutIdentity) {
+        clearTimeout(this.data.refreshCodeTimeoutIdentity);
+      }
+      if (this.data.countdownIntervalIdentity) {
+        clearInterval(this.data.countdownIntervalIdentity);
+      }
+    },
+  },
+});
--- wechat/miniprogram/pages/totp/index.ts
+++ wechat/miniprogram/pages/totp/index.ts
@@ -6,7 +6,7 @@ import { substr } from "@utils/string";
 import type { Tap } from "miniprogram/types/wechat";
 import Message from "tdesign-miniprogram/message/index";
 import Toast from "tdesign-miniprogram/toast/index";
-import type { Item } from "types/totp";
+import type { Item, RefreshCodeFailed } from "types/totp";
 
 interface Dataset {
   id: string;
@@ -16,19 +16,16 @@ Page({
   data: {
     dialogVisible: false,
     dialogDataId: 0,
-    intervalIdentity: -1,
     items: [] as Item[],
   },
   onShow() {
-    this.setupRefreshInterval();
-
     this.all();
   },
   onHide() {
-    this.clearRefreshInterval();
+    // this.clearRefreshInterval();
   },
   onUnload() {
-    this.clearRefreshInterval();
+    // this.clearRefreshInterval();
   },
   all() {
     Toast({
@@ -93,8 +90,6 @@ Page({
   async detail(e: Tap<Dataset, Dataset>) {
     const id = Number(e.currentTarget.dataset.id);
 
-    this.clearRefreshInterval();
-
     await wx.navigateTo({ url: `/pages/totp/detail/index?id=${id}` });
   },
   delete(e: Tap<Dataset, Dataset>) {
@@ -102,20 +97,13 @@ Page({
 
     this.setData({ dialogVisible: true, dialogDataId });
   },
-  refreshCode(id: number, index: number) {
-    api
-      .detail(id)
-      .then((response) =>
-        this.setData({ [`items[${index}].code`]: response.code }),
-      )
-      .catch((e: HttpError) =>
-        Message.error({
-          content: `更新验证码失败：${e.message}`,
-          duration: 5000,
-          offset: [20, 32],
-          context: this,
-        }),
-      );
+  refreshCodeFailed(e: RefreshCodeFailed) {
+    Message.error({
+      content: `更新验证码失败：${e.detail.message}`,
+      duration: 5000,
+      offset: [20, 32],
+      context: this,
+    });
   },
   dialogConfirm(e: Tap<Dataset, Dataset>) {
     const id = Number(e.currentTarget.dataset.id);
@@ -137,34 +125,6 @@ Page({
   },
   dialogCancel() {
     this.setData({ dialogVisible: false, dialogDataId: 0 });
-  },
-  setupRefreshInterval() {
-    const intervalIdentity = setInterval(async () => {
-      for (const item of this.data.items) {
-        const index = this.data.items.indexOf(item);
-        const period = item.config.period ?? 30;
-
-        let remainSeconds = period - new Date().getSeconds();
-        if (remainSeconds <= 0) {
-          remainSeconds += period;
-        }
-
-        this.setData({ [`items[${index}].remainSeconds`]: remainSeconds });
-
-        if (remainSeconds === period) {
-          this.refreshCode(item.id, index);
-        }
-      }
-    }, 1000);
-
-    this.setData({ intervalIdentity });
-  },
-  clearRefreshInterval() {
-    if (this.data.intervalIdentity > 0) {
-      clearInterval(this.data.intervalIdentity);
-    }
-
-    this.setData({ intervalIdentity: -1 });
   },
 });
 
