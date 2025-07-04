7fa71cd2973a201cbf0bf6c2f753f4f3c084fc42
yansongda <me@yansongda.cn>
2025-07-04 18:46:04 +0800

    update

--- database/02-change-bin.sql
+++ database/02-change-bin.sql
@@ -1,31 +1,69 @@
-create user rs_application with password '123';
+-- =============================================
+-- 创建用户和模式
+-- =============================================
+CREATE USER rs_application WITH PASSWORD 'qoCDIv1RVTJycTNQKIaivVWVmRtsTRCr';
 
-create schema account AUTHORIZATION rs_application;
+CREATE SCHEMA account;
+CREATE SCHEMA tool;
 
-create table "account"."access_token" like "miniprogram"."access_token";
-insert into "account"."access_token" select * from "miniprogram"."access_token";
+-- =============================================
+-- 账户模块表创建与数据迁移
+-- =============================================
+BEGIN;
+
+-- access_token 表
+CREATE TABLE account.access_token (LIKE miniprogram.access_token INCLUDING ALL);
+INSERT INTO account.access_token SELECT * FROM miniprogram.access_token;
+CREATE SEQUENCE account.access_token_id_seq;
+ALTER TABLE account.access_token
+    ALTER COLUMN id SET DEFAULT nextval('account.access_token_id_seq');
+SELECT setval('account.access_token_id_seq',
+              COALESCE((SELECT MAX(id) FROM account.access_token), 1));
+
+-- third_user 表
+CREATE TABLE account.third_user (LIKE miniprogram.third_user INCLUDING ALL);
+INSERT INTO account.third_user SELECT * FROM miniprogram.third_user;
+CREATE SEQUENCE account.third_user_id_seq;
+ALTER TABLE account.third_user
+    ALTER COLUMN id SET DEFAULT nextval('account.third_user_id_seq');
+SELECT setval('account.third_user_id_seq',
+              COALESCE((SELECT MAX(id) FROM account.third_user), 1));
+
+-- user 表
+CREATE TABLE account."user" (LIKE miniprogram."user" INCLUDING ALL);
+INSERT INTO account."user" SELECT * FROM miniprogram."user";
+CREATE SEQUENCE account.user_id_seq;
+ALTER TABLE account."user"
+    ALTER COLUMN id SET DEFAULT nextval('account.user_id_seq');
+SELECT setval('account.user_id_seq',
+              COALESCE((SELECT MAX(id) FROM account."user"), 1));
+
+-- =============================================
+-- 工具模块表创建与数据迁移
+-- =============================================
+-- totp 表
+CREATE TABLE tool.totp (LIKE miniprogram.totp INCLUDING ALL);
+INSERT INTO tool.totp SELECT * FROM miniprogram.totp;
+CREATE SEQUENCE tool.totp_id_seq;
+ALTER TABLE tool.totp
+    ALTER COLUMN id SET DEFAULT nextval('tool.totp_id_seq');
+SELECT setval('tool.totp_id_seq',
+              COALESCE((SELECT MAX(id) FROM tool.totp), 1));
+
+-- short_url 表
+CREATE TABLE tool.short_url (LIKE miniprogram.short_url INCLUDING ALL);
+INSERT INTO tool.short_url SELECT * FROM miniprogram.short_url;
+CREATE SEQUENCE tool.short_url_id_seq;
+ALTER TABLE tool.short_url
+    ALTER COLUMN id SET DEFAULT nextval('tool.short_url_id_seq');
+SELECT setval('tool.short_url_id_seq',
+              COALESCE((SELECT MAX(id) FROM tool.short_url), 1));
+
+COMMIT;
+
+-- todo: tool, account owner
 alter table "account"."access_token" owner to rs_application;
-
-create table "account"."third_user" like "miniprogram"."third_user";
-insert into "account"."third_user" select * from "miniprogram"."third_user";
 alter table "account"."third_user" owner to rs_application;
-
-create table "account"."user" like "miniprogram"."user";
-insert into "account"."user" select * from "miniprogram"."user";
 alter table "account"."user" owner to rs_application;
-
-create schema tool AUTHORIZATION rs_application;
-
-create table "tool"."totp" like "miniprogram"."totp";
-insert into "tool"."totp" select * from "miniprogram"."totp";
 alter table "tool"."totp" owner to rs_application;
-
-create table "tool"."short_url" like "miniprogram"."short_url";
-insert into "tool"."short_url" select * from "miniprogram"."short_url";
 alter table "tool"."short_url" owner to rs_application;
-
-select setval('account.access_token_id_seq', nextval('account.access_token_id_seq'))
-select setval('account.user_id_seq', nextval('account.user_id_seq'))
-select setval('account.third_user_id_seq', nextval('account.third_user_id_seq'))
-select setval('tool.short_url_id_seq', nextval('tool.short_url_id_seq'))
-select setval('tool.totp_id_seq', nextval('tool.totp_id_seq'))
