2f7bb9c706fedb568ce93b9a971ff3be6697bcab
yansongda <me@yansongda.cn>
2025-09-21 22:47:10 +0800

    update

--- wechat/miniprogram/constant/app.ts
+++ wechat/miniprogram/constant/app.ts
@@ -6,7 +6,7 @@ const STORAGE = {
 const URL = {
   develop: "http://127.0.0.1:8080",
   release: "https://application.yanda.net.cn",
-  trial: "https://application.yanda.net.cn",
+  trial: "https://application.test.yanda.net.cn",
 };
 
 export { STORAGE, URL };
